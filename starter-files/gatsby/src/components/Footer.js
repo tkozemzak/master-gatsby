05052abf4da4a4c32b34728b43ccc2b7754ffe13
Tim Kozemzak <timkozemzak@gmail.com>
2021-08-31 10:39:08 -0700

    update nav layout. Add typography styled component

--- starter-files/gatsby/src/components/Footer.js
+++ starter-files/gatsby/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; Slick's Slices {new Date().getFullYear()}</p>
+      <p>&copy; Tim Kozemzak {new Date().getFullYear()}</p>
     </footer>
   );
 };
--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -2,10 +2,19 @@ import React from "react";
 import Footer from "./Footer";
 import Nav from "./Nav";
 import "normalize.css";
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from "../styles/Typography";
+import { styled } from 'styled-components';
+
+const ContentStyles = styled.div`
+
+`;
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <GlobalStyles/>
+      <Typography/>
       <Nav />
       {children}
       <Footer />
