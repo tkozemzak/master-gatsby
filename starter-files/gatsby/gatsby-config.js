f1ff0a9164b87fd59b42d709abdccf5fb64d0fef
Tim Kozemzak <timkozemzak@gmail.com>
2021-09-01 15:40:09 -0700

    configure plugins and sanity graphql api

---
+++ starter-files/gatsby/gatsby-config.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+export default {
+  siteMetadata: {
+    title: `Slicks Slices`,
+    description: `Best Pizza west of the Mississippi!`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `x9z7d9dr`,
+        dataset: `production`,
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Helmet from "react-helmet";
 
 const HomePage = () => {
   return (
     <>
+      <Helmet>
+        <title>Slick's Slices</title>
+      </Helmet>
       <h1> Home </h1>
     </>
   );
