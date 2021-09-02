fd61dff843fe69c5ff35c2fc8e3461ce7e5ef6d5
Tim Kozemzak <timkozemzak@gmail.com>
2021-09-02 16:07:03 -0700

    configure gatsby-image and add images for each pizza

--- starter-files/gatsby/src/components/PizzaList.js
+++ starter-files/gatsby/src/components/PizzaList.js
@@ -1,16 +1,17 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
 
 const SinglePizza = ({ pizza }) => {
 	return (
 		<div>
 			<Link to={`/pizza/${pizza.slug.current}`}>
 				<h2>
-					<span className="mark"> {pizza.name} </span>
+					<span className='mark'> {pizza.name} </span>
 				</h2>
 				<p> {pizza.toppings.map((topping) => topping.name).join(', ')} </p>
+				<Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
 			</Link>
-			<img src={pizza.image.asset.fluid.src} />
 		</div>
 	);
 };
